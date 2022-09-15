5de24b0cfc9b748ad019d035feef6b6f43678973
Maxime Leblond <maxime.leblond76@gmail.com>
2022-09-15 23:15:55 +0200

    Update script_iadfrance.user.js
    test if update work

--- script_iadfrance.user.js
+++ script_iadfrance.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.iadfrance.fr/trouver-un-conseiller/*
 // @grant       GM_addStyle
-// @version    1.0
+// @version    1.1
 // @alphadownloadURL https://github.com/depfryer/script-random/raw/main/script_iadfrance.user.js
 // @alphaupdateURL https://github.com/depfryer/script-random/raw/main/script_iadfrance.user.js
 // ==/UserScript==
